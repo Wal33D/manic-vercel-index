6b1fe35c90730f6b2ea0800222e6af8fb730cfc3
Waleed Gudah <aquataze@yahoo.com>
2024-06-05 13:57:48 -0500

    Update api/hello.ts with recent changes

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,21 @@
+import { getHognoseIndex } from "../utils/getHognoseIndex";
+import { createApiResponse } from "../utils/createApiResponse";
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    const url = new URL(req.url as string);
+    const catalog = url.searchParams.get('catalog');
+
+    let data;
+
+    if (catalog === 'hognose' || !catalog) {
+      // Return hognose index if 'hognose' is specified or if no catalog is specified
+      data = await getHognoseIndex();
+    } else {
+      return createApiResponse({ data: 'Catalog not found or unsupported', isError: true, req });
+    }
+
+    return createApiResponse({ data, isError: false, req });
 }
+
+
